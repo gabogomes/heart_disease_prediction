605f7a554697ea2b6a27990a5bc4b6e6ea0d73f1
Gabriel Gomes <gabrielogomes@usp.br>
2022-02-16 01:08:23 -0300

    Took out unused library import

--- python_codes/heart_disease_kfold.py
+++ python_codes/heart_disease_kfold.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB, BernoulliNB
 from sklearn.tree import DecisionTreeClassifier
